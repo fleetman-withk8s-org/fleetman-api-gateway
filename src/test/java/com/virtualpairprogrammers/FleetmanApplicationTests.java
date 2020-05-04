1335355981e808c1cf60b5f6a510223d84045988
Krishan <krishan.shyam@gmail.com>
2020-05-04 19:19:21 +0100

    commenting
    publishing test

--- src/test/java/com/virtualpairprogrammers/FleetmanApplicationTests.java
+++ src/test/java/com/virtualpairprogrammers/FleetmanApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+// this is comment
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 public class FleetmanApplicationTests {
